09f383698ccd70b5fe4bc92627e150dd4ddbdb17
Alex Sagel <alex.sagel7587@gmail.com>
2017-12-28 02:28:33 +0000

    Done.

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,42 @@
-// Code Product Component Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+ export default class Product extends React.Component{
+  render(){
+    return(
+      <div className="dunder">
+        <h3>{this.props.name}</h3>
+        <p>{this.props.producer}</p>
+        <p>{this.props.hasWatermark}</p>
+        <p>{this.props.color}</p>
+        <p>{this.props.weight}</p>
+      </div>
+    );
+  }
+}
+
+
+
+Product.defaultProps = {
+  hasWatermark: false,
+}
+
+var weight_range = [];
+for (var i=80; i<=300; i++){
+  weight_range.push(i);
+}
+
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  producer: PropTypes.string,
+  hasWatermark: PropTypes.bool,
+  color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
+  weight: PropTypes.oneOf(weight_range).isRequired  /*function(props, propName, componentName){
+    let weight = props[propName];
+    if (weight == undefined){
+        return new Error ('Must include wight');
+    }
+    return weight >= 80 && weight <= 300 ? null : new Error("must be within the range!");
+  } */
+};
