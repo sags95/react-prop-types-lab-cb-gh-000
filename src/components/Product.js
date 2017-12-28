946b19c8249085852b08d1728136053692909e5d
Alex Sagel <alex.sagel7587@gmail.com>
2017-12-28 01:48:01 +0000

    automatically backed up by learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,28 @@
 // Code Product Component Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+ export default class Product extends React.Component{
+  render(){
+    return(
+      <div className="dunder">
+        <h3>{this.props.name}</h3>
+        <p>{this.props.producer}</p>
+        <p>{this.props.hasWatermark}</p>
+        <p>{this.props.color}</p>
+        <p>{this.props.weigh}</p>
+      </div>
+    );
+  }
+}
+
+Product.defaultProps = {
+  hasWatermark: false,
+}
+
+Product.PropTypes = {
+  name: PropTypes.string.isRequired,
+  producer: PropTypes.string,
+  hasWatermark: PropTypes.boolean,
+  color: PropTypes.string
+}
